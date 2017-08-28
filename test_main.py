1eab22728f3cceb02a1ff81ea969e2ad026d48aa
Je-Hoon Song <song.jehoon@gmail.com>
2017-08-28 22:32:44 +0900

    Update contents

--- test_main.py
+++ test_main.py
@@ -1,3 +1,6 @@
+# reference
+# http://pythonhosted.org/gseapy/gseapy_example.html#gsea-example
+
 import pandas as pd
 import gseapy as gp
 import matplotlib.pyplot as plt
@@ -7,41 +10,48 @@ from pdb import set_trace
 from numpy import number 
 from os.path import exists
 
+def test(): 
+    classfile = 'GSEApy/data/P53.cls'
+    # 50 2 1
+    # #MUT WT
+    # MUT MUT MUT MUT MUT MUT MUT MUT MUT MUT MUT MUT MUT MUT MUT MUT MUT MUT MUT MUT \
+    # MUT MUT MUT MUT MUT MUT MUT MUT MUT MUT MUT MUT MUT WT WT WT WT WT WT WT WT WT \ 
+    # WT WT WT WT WT WT WT WT
 
-df1 = dataset_ccle.load_gexp()
-df2 = df1[df1.columns[1:4]]
-df3 = df2 
+    geneexpfile = "GSEApy/data/P53_resampling_data.txt"
+    #            NAME   786-0  BT-549  CCRF-CEM  COLO 205    EKVX  HCC-2998  HCT-15  \
+    # 0        CTLA2B  111.19   86.22    121.85     75.19  208.62    130.59  124.72
+    # 1        SCARA3  460.30  558.34    183.55     37.29  158.00     43.61   80.83
+    # 2  LOC100044683   97.25  118.94     81.17    119.51  119.88    107.73  165.57
+    # 3          CMBL   33.45   55.10    221.67     50.30   35.12     75.70   84.01
+    # 4         CLIC6   35.75   41.26     63.04    219.86   42.53     54.19   86.98
 
-df3 = df3.dropna()
+    phenoA,phenoB, class_vector =  gp.parser.gsea_cls_parser(classfile)
 
-df3 = df3.loc[df2.index[0:500]]
+    gene_exp = pd.read_table(geneexpfile)
 
-cls_vector = ['MUT' for x in range(1)]
-cls_vector += ['WT' for x in range(1)]
+    # gene_exp.head()
 
-# xxx
+    gs_res = gp.gsea(
+        data=gene_exp,
+        gene_sets='KEGG_2016',
+        # cls=class_vector,
+        cls=['Control' for i in range(25)] + ['Drug Treatment' for i in range(25)], 
+        permutation_type='phenotype',
+        outdir='output',
+        method='signal_to_noise',
+        format='png'
+        )
 
-# set_trace()
+    gsea_results= gs_res.res2d
+    # gs_res.res2d.head()
 
-df = df3 
+    with plt.style.context('ggplot'):
+        gsea_results = gsea_results.reset_index()
+        gsea_results.head(5).plot.barh(y='fdr',x='Term',fontsize=10)
 
-# df.drop_duplicates(subset=df.columns[0], inplace=True) #drop duplicate gene_names.
-# df.set_index(keys=df.columns[0], inplace=True)
-# df.dropna(how='all', inplace=True)                     #drop rows with all NAs
-# df2 = df.select_dtypes(include=[number])
-# #drop any genes which std ==0
+        plt.savefig('figure-gsea.pdf')
 
-# # set_trace() 
-# df_std =  df2.groupby(by=cls_vector, axis=1).std()
-# df2 =  df2[~df_std.isin([0]).any(axis=1)]
-# df2 = df2 + 0.00001 # we don't like zeros!!!
+    # xxx
+   
 
-# run call
-# enrichr library are supported by call module. Just provide the name
-# you may also provide a gene_sets file in gmt format, just like GSEA do.
-gs_res = gp.gsea(data=df3, gene_sets='KEGG_2016', cls=cls_vector, 
-                 permutation_type='phenotype',
-                 outdir='gsea_reprot', method='signal_to_noise', format='png')
-
-
-xxx
